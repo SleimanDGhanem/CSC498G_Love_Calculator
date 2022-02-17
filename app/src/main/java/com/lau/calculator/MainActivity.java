c18ed1a471c13f0c83afa84de439aecef93fccad
Sleiman Ghanem <sleiman.ghanem@outlook.com>
2022-02-17 02:18:24 +0200

    Added table and the bare ability to dynamically add rows to it

--- app/src/main/java/com/lau/calculator/MainActivity.java
+++ app/src/main/java/com/lau/calculator/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -36,11 +42,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+
+
     public void calculate(View v){
         Spinner spinner = findViewById(R.id.spinner1);
         ImageView img = (ImageView) findViewById(R.id.img);
-
+        TextView t = (TextView) findViewById(R.id.txt);
         String text = spinner.getSelectedItem().toString();
+        Random rand = new Random();
+        t.setText(Integer.toString(rand.nextInt(101)));
+        EditText name = (EditText) findViewById(R.id.name);
+
+        TableLayout table = (TableLayout) findViewById(R.id.table);
+        TableRow tb = new TableRow(this);
+        TextView tv1 = new TextView(this);
+        TextView tv2 = new TextView(this);
+        TextView tv3 = new TextView(this);
+        tv1.setText(name.getText().toString());
+        tv2.setText(text);
+        tv3.setText();
+        tb.addView(tv1);
+        table.addView(tb);
 
         switch (text){
 
